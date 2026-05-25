fc30f9425a77c1ac1ba895bf717a1e930b69f646
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2026-05-25 17:00:06 -0300

    serialize / deserialize Comments

--- src/comment/dto/comment-response.dto.ts
+++ src/comment/dto/comment-response.dto.ts
@@ -1,11 +1,31 @@
 import { AssociatedType } from '../model/associated-type.enum';
+import { Expose } from 'class-transformer';
+import { CommentModel } from '../model/comment.model';
+import { plainToInstance } from 'class-transformer';
 
 export class CommentResponseDto {
+  @Expose()
   id!: number;
+  @Expose()
   content!: string;
+  @Expose()
   createdAt!: Date;
+  @Expose()
   createdBy!: number;
+  @Expose()
   associatedTo!: number;
+  @Expose()
   associatedType!: AssociatedType;
+  @Expose()
   likedBy!: number[];
+
+  static from(model: CommentModel): CommentResponseDto {
+    return plainToInstance(CommentResponseDto, model, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  static fromMany(models: CommentModel[]): CommentResponseDto[] {
+    return models.map((model) => CommentResponseDto.from(model));
+  }
 }
