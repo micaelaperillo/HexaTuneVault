91c4e384bf5b300832ad8c03c65faebd3ca3bee4
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2026-05-25 14:38:51 -0300

    match type as well

--- src/comment/repository/i-comment.repository.ts
+++ src/comment/repository/i-comment.repository.ts
@@ -1,3 +1,4 @@
+import { AssociatedType } from '../model/associated-type.enum';
 import { CommentFilters } from '../model/comment-filters.model';
 import { CommentModel } from '../model/comment.model';
 
@@ -7,7 +8,7 @@ export interface ICommentRepository {
     create(comment: Omit<CommentModel, 'id'>): Promise<CommentModel>;
     findById(id: number): Promise<CommentModel | null>;
     search(filters: CommentFilters): Promise<CommentModel[]>;
-    findByAssociatedId(associatedId: number): Promise<CommentModel[]>;
+    findByAssociatedId(associatedId: number, associatedType: AssociatedType): Promise<CommentModel[]>;
     deleteById(id: number): Promise<void>;
     addLike(commentId: number, userId: number): Promise<void>;
     removeLike(commentId: number, userId: number): Promise<void>;
