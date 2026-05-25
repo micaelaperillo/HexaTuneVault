843fe3f71dac434cb187cb26790286e9301235fc
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2026-05-25 15:44:02 -0300

    initial adapter implementation

---
+++ src/comment/adapter/comment.repository.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm/browser/repository/Repository.js';
+import { CommentEntity } from '../entity/comment.entity';
+import { ICommentRepository } from '../repository/i-comment.repository';
+import { AssociatedType } from '../model/associated-type.enum';
+import { CommentModel } from '../model/comment.model';
+import { CommentFilters } from '../model/comment-filters.model';
+
+@Injectable()
+export class CommentRepository implements ICommentRepository {
+  constructor(
+    @InjectRepository(CommentEntity)
+    private repo: Repository<CommentEntity>,
+  ) {}
+
+  async create(comment: Omit<CommentEntity, 'id'>): Promise<CommentEntity> {
+    return this.repo.save(comment);
+  }
+
+  async findById(id: number): Promise<CommentEntity | null> {
+    return this.repo.findOneBy({ id });
+  }
+
+  async deleteById(id: number): Promise<void> {
+    await this.repo.delete(id);
+  }
+
+  async findByAssociatedId(
+    associatedId: number,
+    associatedType: AssociatedType,
+  ): Promise<CommentModel[]> {
+    return this.repo.findBy({ associatedTo: associatedId, associatedType });
+  }
+
+  async addLike(commentId: number, userId: number): Promise<void> {
+    const comment = await this.repo.findOneBy({ id: commentId });
+    if (comment && !comment.likedBy.includes(userId)) {
+      comment.likedBy.push(userId);
+      await this.repo.save(comment);
+    }
+  }
+
+  async removeLike(commentId: number, userId: number): Promise<void> {
+    const comment = await this.repo.findOneBy({ id: commentId });
+    if (comment) {
+      comment.likedBy = comment.likedBy.filter((id) => id !== userId);
+      await this.repo.save(comment);
+    }
+  }
+
+  async search(filters: CommentFilters): Promise<CommentModel[]> {
+    const qb = this.repo.createQueryBuilder('comment');
+
+    if (filters.createdBy !== undefined)
+      qb.andWhere('comment.createdBy = :createdBy', {
+        createdBy: filters.createdBy,
+      });
+    if (filters.content !== undefined)
+      qb.andWhere('comment.content ILIKE :content', {
+        content: `%${filters.content}%`,
+      });
+    if (filters.associatedType !== undefined)
+      qb.andWhere('comment.associatedType = :associatedType', {
+        associatedType: filters.associatedType,
+      });
+
+    return qb.getMany();
+  }
+}
