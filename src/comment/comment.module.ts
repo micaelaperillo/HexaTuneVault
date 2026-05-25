1675862fda464b7b9126129ca149f70f7764a8ec
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2026-05-25 17:02:50 -0300

    comments module exposed

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,4 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommentEntity } from './entity/comment.entity';
+import { CommentService } from './service/comment.service';
+import { COMMENT_REPOSITORY } from './repository/i-comment.repository';
+import { CREATE_COMMENT } from './port/i-create-comment.port';
+import { DELETE_COMMENT } from './port/i-delete-comment.port';
+import { LIKE_COMMENT } from './port/i-like-comment-port';
+import { UNLIKE_COMMENT } from './port/i-unlike-comment.port';
+import { GET_COMMENT } from './port/i-get-comment.port';
+import { SEARCH_COMMENT } from './port/i-search-comment.port';
+import { CommentRepository } from './adapter/comment.repository';
+import { CommentController } from './adapter/comment.controller';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([CommentEntity])],
+  controllers: [CommentController],
+  providers: [
+    CommentService,
+    { provide: COMMENT_REPOSITORY, useClass: CommentRepository },
+    { provide: CREATE_COMMENT, useExisting: CommentService },
+    { provide: DELETE_COMMENT, useExisting: CommentService },
+    { provide: SEARCH_COMMENT, useExisting: CommentService },
+    { provide: GET_COMMENT, useExisting: CommentService },
+    { provide: LIKE_COMMENT, useExisting: CommentService },
+    { provide: UNLIKE_COMMENT, useExisting: CommentService },
+  ],
+})
 export class CommentModule {}
