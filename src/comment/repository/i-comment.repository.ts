c1d8a1a116e6fc9dd6cd7344093e931dba5792c8
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2026-05-25 12:23:26 -0300

    initial icomments repo

---
+++ src/comment/repository/i-comment.repository.ts
@@ -0,0 +1,14 @@
+import { CommentFilters } from '../model/comment-filters.model';
+import { CommentModel } from '../model/comment.model';
+
+export const COMMENT_REPOSITORY = Symbol('ICommentRepository');
+
+export interface ICommentRepository {
+    create(comment: Omit<CommentModel, 'id'>): Promise<CommentModel>;
+    findById(id: number): Promise<CommentModel | null>;
+    search(filters: CommentFilters): Promise<CommentModel[]>;
+    findByAssociatedId(associatedId: number): Promise<CommentModel[]>;
+    deleteById(id: number): Promise<void>;
+    addLike(commentId: number, userId: number): Promise<void>;
+    removeLike(commentId: number, userId: number): Promise<void>;
+}
