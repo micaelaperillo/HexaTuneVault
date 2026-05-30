b0f904f25b220aff42f7d808fead0e302e77ed64
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2026-05-30 15:42:04 -0300

    HATEOAS comment response dto

--- src/dto/comment-response.dto.ts
+++ src/dto/comment-response.dto.ts
@@ -1,27 +1,41 @@
-import { AssociatedType } from '../model/associated-type.enum';
+import { AssociatedType } from '../model/associated-type';
 import { Expose, plainToInstance } from 'class-transformer';
 import { CommentModel } from '../model/comment.model';
 
 export class CommentResponseDto {
   @Expose()
-  id!: number;
-  @Expose()
   content!: string;
   @Expose()
   createdAt!: Date;
   @Expose()
-  createdBy!: string;
+  associatedType!: AssociatedType;
   @Expose()
-  associatedTo!: string;
+  self!: `/${string}`;
   @Expose()
-  associatedType!: typeof AssociatedType;
+  like!: `/${string}`;
   @Expose()
-  likedBy!: string[];
+  unlike!: `/${string}`;
+  @Expose()
+  collection!: `/${string}`;
+  @Expose()
+  associated!: `/${string}`;
+  @Expose()
+  author!: `/${string}`;
+  @Expose()
+  likes!: `/${string}`;
 
   static from(model: CommentModel): CommentResponseDto {
-    return plainToInstance(CommentResponseDto, model, {
+    const dto = plainToInstance(CommentResponseDto, model, {
       excludeExtraneousValues: true,
     });
+    dto.self = `/api/comments/${model.id}`;
+    dto.like = `/api/comments/${model.id}/like`;
+    dto.unlike = `/api/comments/${model.id}/unlike`;
+    dto.collection = `/api/comments`;
+    dto.associated = `/api/${model.associatedType}s/${model.associatedTo}`;
+    dto.author = `/api/users/${model.createdBy}`;
+    dto.likes = `/api/comments/${model.id}/likes`;
+    return dto;
   }
 
   static fromMany(models: CommentModel[]): CommentResponseDto[] {
