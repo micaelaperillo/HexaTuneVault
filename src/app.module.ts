6ce035afa266770096e15c5396b1f8bb5ffbc313
Gianfranco Magliotti <95419759+GMagliotti@users.noreply.github.com>
2026-05-30 08:30:15 -0300

    feat: wire and register user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ArtistModule } from './module/artist.module';
 import { CommentModule } from './module/comment.module';
+import { UserModule } from './module/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ArtistModule,
     CommentModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
