05d6b9075af8085ec926b632719babe74089cf56
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-05-31 04:30:31 -0300

    Improve image controller

--- src/controller/image.controller.ts
+++ src/controller/image.controller.ts
@@ -2,7 +2,6 @@ import type { Express, Response } from 'express';
 
 import {
   Controller,
-  HttpCode,
   Logger,
   ParseFilePipeBuilder,
   Post,
@@ -18,19 +17,19 @@ export class ImageController {
   private readonly logger = new Logger(ImageController.name);
 
   @Post()
-  @HttpCode(204)
   @UseInterceptors(FileInterceptor('img'))
   create(
     @UploadedFile(
       new ParseFilePipeBuilder()
         // 10mb
         .addMaxSizeValidator({ maxSize: 10_000_000 })
+        .addFileTypeValidator({ fileType: /^image\/(png|jpeg|gif)$/ })
         .build(),
     )
     file: Express.Multer.File,
     @Res({ passthrough: true }) res: Response,
   ) {
-    this.logger.log(file);
+    this.logger.log(`Uploaded ${file.filename} (${file.size} bytes)`);
     res.location(`/user-content/${file.filename}`);
   }
 }
