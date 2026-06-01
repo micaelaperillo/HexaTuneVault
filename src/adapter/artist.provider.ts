eb3a8c39ed0d14b48ddadd7d89be4cc59f7c9a8e
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-06-01 01:27:56 -0300

    Fix artist's search bug

--- src/adapter/artist.provider.ts
+++ src/adapter/artist.provider.ts
@@ -44,10 +44,11 @@ export class SpotifyArtistProvider implements IArtistProvider {
   }
 
   private static toQuery(filters: ArtistFilters) {
+    const token = ' genre:';
     // TODO: Fix injection vulnerability
     return (
       filters.name +
-      (filters.genre?.length ? `,tag:${filters.genre?.join(',tag:')}` : '')
+      (filters.genre?.length ? `${token}${filters.genre?.join(token)}` : '')
     );
   }
 
