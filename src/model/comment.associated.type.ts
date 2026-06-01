f7f1b5ed366036212b0688fec7418f553d3fadab
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2026-05-31 21:31:31 -0300

    will only handle review and comment

--- src/model/comment.associated.type.ts
+++ src/model/comment.associated.type.ts
@@ -1,10 +1,6 @@
 export const AssociatedType = {
-  TRACK: 'track',
-  ALBUM: 'album',
-  PODCAST: 'podcast',
   REVIEW: 'review',
   COMMENT: 'comment',
-  ARTIST: 'artist',
 } as const;
 
 export type AssociatedType =
